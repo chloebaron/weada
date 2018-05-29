03fcdc1ff2a1a574fc308958a959863d35637b81
Chloe Baron <chloe.f.baron@gmail.com>
2018-05-29 19:05:32 -0400

    API working, tests still to be completed

---
+++ app/policies/calendar_policy.rb
@@ -0,0 +1,17 @@
+class CalendarPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope
+    end
+  end
+
+  def show?
+    true
+  end
+
+  def callback?
+    true
+  end
+
+
+end
