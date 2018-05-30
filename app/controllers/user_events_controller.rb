420aa490e9b2773564bac8f2bb99ab4baf46550d
Chloe Baron <chloe.f.baron@gmail.com>
2018-05-30 18:44:18 -0400

    Some things with cards, still to work on integrating checkbox selection

--- app/controllers/user_events_controller.rb
+++ app/controllers/user_events_controller.rb
@@ -1,18 +1,31 @@
-class ActivitiesController < ApplicationController
+class UserEventsController < ApplicationController
   # skip_before_action :authenticate_user!, only: [:index]
 
   def new
+    @user_event = UserEvent.new
   end
 
   def create
+    @user_event = UserEvent.new(event_params)
+    if @user_event.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
-  def edit
-  end
+  # def edit
+  # end
 
-  def update
-  end
+  # def update
+  # end
 
-  def destroy
+  # def destroy
+  # end
+
+  private
+
+  def event_params
+    params.require(:user_event).permit(:name, :duration)
   end
 end
