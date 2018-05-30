a388a12325c2f8930981fb72a37526eb22f8e303
Chloe Baron <chloe.f.baron@gmail.com>
2018-05-30 17:19:03 -0400

    Continuing with HTML and erb

---
+++ app/controllers/user_events_controller.rb
@@ -0,0 +1,20 @@
+class ActivitiesController < ApplicationController
+  # skip_before_action :authenticate_user!, only: [:index]
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,12 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  resources :activities, only: [:index]
+
+  resources :user_events, only: [:new, :create, :edit, :update, :destroy]
+
   get '/calendar', to: 'calendars#show'
+
   get '/redirect', to: 'calendars#redirect', as: 'redirect'
   get '/callback', to: 'calendars#callback', as: 'callback'
 
