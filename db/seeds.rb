b3aedb0c3c8e366658dfb4fb0019c30a6425f719
leo marshall <tgbrtmarshall@gmail.com>
2018-05-29 21:26:41 -0400

    added get_get_weather_from json method instead of saving weather conditions to db. Now there is a global variable with the json data instead within the weatherconditions controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,3 @@
-require 'open-uri'
-require 'json'
-
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -14,9 +11,8 @@ require 'json'
 # User.create(email: "user@user.com", password: "testing")
 Activity.destroy_all
 User.destroy_all
-WeatherCondition.destroy_all
 
-Activity.create(name: "joggling", category: "Outdoor")
+Activity.create(name: "jogging", category: "Outdoor")
 
 User.create!(
   email: "test1@test.com",
@@ -26,29 +22,30 @@ User.create!(
   address: " 5333 Avenue Casgrain #102, Montréal, QC H2T 1X6".strip.gsub(/\s+/, " ").gsub(/(\(|\)|\#)/, "").unicode_normalize(:nfkd).encode('ASCII', replace: '')
   )
 
-user_address = User.first.address
+# user_address = User.first.address
 
-geo_url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{user_address}&key=#{ENV["GEOCODE_API_LEO"]}"
-geo_json = open(geo_url).read
-geocode = JSON.parse(geo_json)
-geo_location = geocode["results"][0]["geometry"]["location"]
+# geo_url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{user_address}&key=#{ENV["GOOGLE_API_LEO"]}"
+# geo_json = open(geo_url).read
+# geocode = JSON.parse(geo_json)
+# geo_location = geocode["results"][0]["geometry"]["location"]
 
-weather_url = "https://api.darksky.net/forecast/#{ENV["DARKSKY_API_LEO"]}/#{geo_location['lat']},#{geo_location['lng']}"
-weather_json = open(weather_url).read
-weather = JSON.parse(weather_json)
+# weather_url = "https://api.darksky.net/forecast/#{ENV["DARKSKY_API_LEO"]}/#{geo_location['lat']},#{geo_location['lng']}"
+# weather_json = open(weather_url).read
+# weather = JSON.parse(weather_json)
 
-seven_day_cycle = [weather["daily"]["data"]]
+# seven_day_cycle = [weather["daily"]["data"]]
 
-seven_day_cycle[0].each do |weather_condition|
-  date_day = Time.at(weather_condition["time"]).day
-  WeatherCondition.create!(
-  location: user_address,
-  temperature: weather_condition["temperatureHigh"],
-  apparent_temperature: weather_condition["apparentTemperatureHigh"],
-  cloud_cover: weather_condition["cloudCover"].to_f,
-  wind_speed: weather_condition["windSpeed"].to_f,
-  precip_probability: weather_condition["precipProbability"].to_f,
-  precip_type: weather_condition["precipType"],
-  day: date_day
-  )
-end
+# seven_day_cycle[0].each do |weather_condition|
+#   date_day = Time.at(weather_condition["time"]).day
+#   WeatherCondition.create!(
+#   location: user_address,
+#   temperature: weather_condition["temperatureHigh"],
+#   apparent_temperature: weather_condition["apparentTemperatureHigh"],
+#   cloud_cover: weather_condition["cloudCover"].to_f,
+#   wind_speed: weather_condition["windSpeed"].to_f,
+#   precip_probability: weather_condition["precipProbability"].to_f,
+#   precip_type: weather_condition["precipType"],
+#   day: date_day
+#   )
+# end
+
