6f6bfca074158501f660d5850f4d9325cffd7027
Linming Ye <leaves.woods@hotmail.com>
2018-06-03 02:29:21 -0400

    fixed a critical issue

--- app/controllers/user_events_controller.rb
+++ app/controllers/user_events_controller.rb
@@ -7,6 +7,8 @@ class UserEventsController < ApplicationController
   end
 
   def create
+    # Destroy last pending user_events records
+    UserEvent.where(status: 0).destroy_all
     params[:activity_ids].each do |activity_id|
       activity = Activity.find(activity_id)
       UserEvent.create(user: current_user, activity: activity, status: 0)
